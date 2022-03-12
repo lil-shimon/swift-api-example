0bfe3b28977f0afe6b67e3f1b194e951e6e0a95f
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-03-13 02:43:48 +0900

    api call

--- swift-api-example/ContentView.swift
+++ swift-api-example/ContentView.swift
@@ -7,13 +7,71 @@
 
 import SwiftUI
 
+// Course Structure
+struct Course: Hashable, Codable {
+    let name: String
+    let image: String
+}
+
+class ViewModel: ObservableObject {
+    // stateみたいなもん
+    // 変更をUIに通知できる
+    @Published var courses: [Course] = []
+    
+    func fetch() {
+        
+        /// api url
+        guard let url = URL(string:
+                                "https://iosacademy.io/api/v1/courses/index.php") else {
+            return
+        }
+        
+        // call api
+        let task = URLSession.shared.dataTask(with: url) { [weak self]data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                /// Json decord
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                
+                /// Queue for ui update
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
+
 struct ContentView: View {
+    /// craete instance
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         NavigationView {
             List {
-                
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack {
+                        Image("")
+                            .frame(width: 130, height: 70)
+                            .background(Color.gray)
+                        
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
             }
             .navigationTitle("Cources")
+            .onAppear{
+                viewModel.fetch()
+            }
         }
     }
 }
