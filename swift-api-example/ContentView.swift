e290bb36ee1c2cf1ef47be2c943b6f4b98a48070
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-03-13 02:15:31 +0900

    Initial Commit

---
+++ swift-api-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-api-example
+//
+//  Created by 下澤健太 on 2022/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-api-example/swift_api_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_api_exampleApp.swift
+//  swift-api-example
+//
+//  Created by 下澤健太 on 2022/03/13.
+//
+
+import SwiftUI
+
+@main
+struct swift_api_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
